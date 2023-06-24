6afe1b3bf8d991bca72c37cb8068c9bddf589597
htootintwai <htootintwai1757@gmail.com>
2023-06-24 21:43:38 +0630

    Htoo Tint Wai - Portfolio

--- Portfolio Assets/JS/cus.js
+++ Portfolio Assets/JS/cus.js
@@ -98,12 +98,31 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
 });
 
 //Mobile View
-function toggleMobileMenu() {
-  const mobileMenu = document.getElementById('mobileMenu');
-  const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
-  mobileMenu.classList.toggle('open');
-  mobileMenuToggle.classList.toggle('open');
-}
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s `;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+  //
+};
+
+navSlide();
+
+
 
 window.addEventListener('resize', removeTextJustify);
 
